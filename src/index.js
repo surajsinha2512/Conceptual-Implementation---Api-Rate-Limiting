2d6b6de3e78b41c01785eac006f5d7a84863f88c
surajsinha2512 <61259615+surajsinha2512@users.noreply.github.com>
2021-01-06 11:18:49 +0530

    push

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
+const posts = require('./initialData');
 const port = 3000
 app.use(express.urlencoded());
-const posts=require('./initialData');
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
@@ -12,39 +12,42 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
-// your code goes here
-let numberofApiCalls=0;
-let initialMax=null;
-app.get('/api/posts',(req,res)=>{
+// your code goes here 
 
-    if(numberofApiCalls>5){
-        res.status(429).send({message:"Exceed Number of API Calls"})
-       return ;
+let numOfApiCalls = 0;
+let initialMax = null;
+
+
+app.get('/api/posts', (req,res) => {
+    if(numOfApiCalls >= 5) {
+        res.status(429).send({message: "Exceed Number of API Calls"});
+        return;
     }
 
-    const parsedMax=Number(req.query.max||10)
-    const max=parsedMax>20?10:parsedMax;
-    let finalMax=max;
-    if(initialMax==null){
-        finalMax=Math.max(finalMax,initialMax)
 
-    }
-    const topMax=posts.filter((value,Idx)=>{
-    Idx<max
-    })
-    res.send(topMax);
-    if(initialmax==null){
-        initialMax==max;
-        numberofApiCalls++;
-        setTimeout(()=>{
-          initialMax=null;
-          numberofApiCalls=0;
-        },3*10000)
-    }else{
-        numberofApiCalls++;
-    }
-})
+    const parsedMax = Number(req.query.max || 10);
+    const max = parsedMax > 20 ? 10: parsedMax;
+    let finalMAx = max;
 
+    if(initialMax !== null) {
+        finalMAx = Math.min(finalMAx,initialMax);
+    }
+
+    const topmax = posts.filter((value,idx) => idx < finalMAx);
+    res.send(topmax);
+
+    if(initialMax === null) {
+        initialMax = max;
+        numOfApiCalls++;
+        setTimeout(() => {
+            initialMax = null;
+            numOfApiCalls = 0;
+        }, 30*1000);
+    } else {
+        numOfApiCalls++;
+    }
+
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
